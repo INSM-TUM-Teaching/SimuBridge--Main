29ef5ba360f0c17da522facc1c93202f2f53ed8f
Sshahary <Shaharyarnawaz9@gmail.com>
2025-11-11 18:02:27 +0100

    EditorSidebar: redirect the back button to timetable overview

--- frontend/src/components/EditorSidebar/EditorSidebar.jsx
+++ frontend/src/components/EditorSidebar/EditorSidebar.jsx
@@ -1,10 +1,8 @@
-import React from 'react'
+import React from 'react';
 import Sidebar from '../Sidebar';
 
-import {
-    Text,
-  } from '@chakra-ui/react'
-
+import { Text } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 import TypeSelector from './Modelbased/TypeSelector';
 import AddResource from './ResourcesBased/AddResource';
@@ -12,38 +10,87 @@ import AddRole from './ResourcesBased/AddRole';
 import EditResource from './ResourcesBased/EditResource';
 import EditRole from './ResourcesBased/EditRole';
 
-
-const Title = ({text}) => {
-    return <Text fontSize={{base: "xs", md:"sm"}} textAlign="center" color="RGBA(0, 0, 0, 0.80)" fontWeight="bold" textTransform="uppercase">{text}</Text>            
+const Title = ({ text }) => {
+  return (
+    <Text
+      fontSize={{ base: 'xs', md: 'sm' }}
+      textAlign="center"
+      color="RGBA(0, 0, 0, 0.80)"
+      fontWeight="bold"
+      textTransform="uppercase"
+    >
+      {text}
+    </Text>
+  );
 };
 
 //TODO get rid of this type of sidebar management
 function EditorSidebar(props) {
-
-
-    const SelectEditor = () =>{
-        switch (props.current) {
-            case "Resource Parameters": return <EditResource currentResource={props.currentResource} setResource={props.setResource} getData={props.getData} setCurrent={props.setCurrent}/>
-            case "Resource Parameters for Roles": return <EditRole currentRole={props.currentRole} setRole={props.setRole} getData={props.getData} setCurrent={props.setCurrent}/>
-            case "Add Resource": return <AddResource getData={props.getData}  setCurrent={props.setCurrent}  />
-            case "Add Role": return <AddRole getData={props.getData}  setCurrent={props.setCurrent}  />
-            default:
-                <></>
-        }
+  const navigate = useNavigate();
+  const backToTimetable = () => {
+    try {
+      navigate('/resource/timetable');
+    } catch (e) {
+      // ignore
     }
-    return (
-        <>
-            <Sidebar side="right" backgroundColor="#F0F0F1" title={<Title text={props.current}/>} content={<SelectEditor/>} />
-        </>
-    )
+  };
+
+  const SelectEditor = () => {
+    switch (props.current) {
+      case 'Resource Parameters':
+        return (
+          <EditResource
+            currentResource={props.currentResource}
+            setResource={props.setResource}
+            getData={props.getData}
+            setCurrent={props.setCurrent}
+          />
+        );
+      case 'Resource Parameters for Roles':
+        return (
+          <EditRole
+            currentRole={props.currentRole}
+            setRole={props.setRole}
+            getData={props.getData}
+            setCurrent={props.setCurrent}
+          />
+        );
+      case 'Add Resource':
+        return (
+          <AddResource getData={props.getData} setCurrent={props.setCurrent} />
+        );
+      case 'Add Role':
+        return (
+          <AddRole getData={props.getData} setCurrent={props.setCurrent} />
+        );
+      default:
+        <></>;
+    }
+  };
+  return (
+    <>
+      <Sidebar
+        side="right"
+        backgroundColor="#FAFBFC"
+        onToggle={backToTimetable}
+        title={<Title text={props.current} />}
+        content={<SelectEditor />}
+      />
+    </>
+  );
 }
 
-export function EditorSidebarAlternate({content, title}) {
-    return (
-        <>
-            <Sidebar side="right" backgroundColor="#F0F0F1" title={<Title text={title}/>} content={content} />
-        </>
-    )
+export function EditorSidebarAlternate({ content, title }) {
+  return (
+    <>
+      <Sidebar
+        side="right"
+        backgroundColor="#FAFBFC"
+        title={<Title text={title} />}
+        content={content}
+      />
+    </>
+  );
 }
 
 export default EditorSidebar;
